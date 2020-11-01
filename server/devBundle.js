6da7da5e30889d0a143e0bb27f8678dda9f49c2a
yokomon <marowmars@gmail.com>
2020-11-01 17:48:40 +0100

    development bundle

---
+++ server/devBundle.js
@@ -0,0 +1,18 @@
+import webpack from "webpack";
+import webpackMiddleware from "webpack-dev-middleware";
+import webpackHotMiddleware from "webpack-hot-middleware";
+import webpackConfig from "./../webpack.config.client";
+import envConfig from "./../config/config";
+
+const webpackCompile = (app) => {
+  if (envConfig.NODE_ENV === "development") {
+    const compiler = webpack(webpackConfig);
+    const middleware = webpackMiddleware(compiler, {
+      publicPath: webpackConfig.output.publicPath,
+    });
+    app.use(middleware);
+    ap.use(webpackHotMiddleware(compiler));
+  }
+};
+
+export { webpackCompile };
