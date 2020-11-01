ce2806e29c3ec35e2c70c5d15dbe8e0f22248cf9
yokomon <marowmars@gmail.com>
2020-11-01 16:37:40 +0100

    config string

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 export default {
   mongoUri: process.env.MONGO_URI || "mongodb://localhost:27017/init_inc",
   port: process.env.PROD_PORT || 3000,
+  jwtSecret: process.env.JWT_SECRET || "mySecretIsHardenoough",
 };
