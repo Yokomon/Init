45ecb0a1ea3fc1fd327abd89cb6a1ea2da6e61c1
yokomon <marowmars@gmail.com>
2020-11-01 17:48:23 +0100

    node env added

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 export default {
+  NODE_ENV: process.env.NODE_ENV || "development",
   mongoUri: process.env.MONGO_URI || "mongodb://localhost:27017/init_inc",
   port: process.env.PROD_PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "mySecretIsHardenoough",
