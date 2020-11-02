ae29c0ea5d08fd9c3c39b83dab99d4d74c2254c5
yokomon <marowmars@gmail.com>
2020-11-02 18:32:04 +0100

    Mui theme import

--- client/App.js
+++ client/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { hot } from "react-hot-loader";
 import MainRouter from "./MainRouter";
 import { BrowserRouter } from "react-router-dom";
-import theme from './theme'
+import theme from "./theme";
 import ThemeProvider from "@material-ui/styles/ThemeProvider";
 
 const App = () => {
