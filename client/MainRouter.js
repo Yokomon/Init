fc32604357df2c8b68b93790c433f5dff5b6cbc3
yokomon <marowmars@gmail.com>
2020-11-02 21:41:16 +0100

    signup route added

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./core/Home";
 import Users from "./user/Users";
+import SignUp from "./user/SignUp";
 
 export default function MainRouter() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/users" component={Users} />
+      <Route path="/signup" component={SignUp} />
     </Switch>
   );
 }
