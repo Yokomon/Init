c125e5e62e4f3e6023e4901a239e4b6f126bb5e8
yokomon <marowmars@gmail.com>
2020-11-02 18:32:30 +0100

    Users component added

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./core/Home";
+import Users from "./user/Users";
 
 export default function MainRouter() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route path="/users" component={Users} />
     </Switch>
   );
 }
