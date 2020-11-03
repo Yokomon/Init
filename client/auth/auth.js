d097ebc9eb24b52e44023bb914dfdae724194998
yokomon <marowmars@gmail.com>
2020-11-03 20:31:21 +0100

    auth client

--- client/auth/auth.js
+++ client/auth/auth.js
@@ -1,9 +1,17 @@
-// Auth mechanism
 const authenticate = (jwt, cb) => {
   if (typeof window !== "undefined") {
     localStorage.setItem("jwt", JSON.stringify(jwt));
   }
   cb();
+};
+
+const isAuthenticated = () => {
+  if (typeof window === "undefined") return false;
+  if (localStorage.getItem("jwt")) {
+    return JSON.parse(localStorage.getItem("jwt"));
+  } else {
+    return false;
+  }
 };
 
 export { authenticate };
