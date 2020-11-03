9873e4293d9dce7d8aac0c23e314c98b3199fd7a
yokomon <marowmars@gmail.com>
2020-11-03 20:52:54 +0100

    Route: signin and profile

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -3,6 +3,8 @@ import { Switch, Route } from "react-router-dom";
 import Home from "./core/Home";
 import Users from "./user/Users";
 import SignUp from "./user/SignUp";
+import SignIn from "./auth/SignIn";
+import Profile from "./user/Profile";
 
 export default function MainRouter() {
   return (
@@ -10,6 +12,8 @@ export default function MainRouter() {
       <Route exact path="/" component={Home} />
       <Route path="/users" component={Users} />
       <Route path="/signup" component={SignUp} />
+      <Route path="/signin" component={SignIn} />
+      <Route path="/user/:userId" component={Profile} />
     </Switch>
   );
 }
