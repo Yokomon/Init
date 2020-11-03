4195d74f780679a800d1b49bad596276c0774441
yokomon <marowmars@gmail.com>
2020-11-03 22:06:48 +0100

    route: private route and edit profile comp

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -5,6 +5,8 @@ import Users from "./user/Users";
 import SignUp from "./user/SignUp";
 import SignIn from "./auth/SignIn";
 import Profile from "./user/Profile";
+import PrivateRoute from "./auth/PrivateRoute";
+import EditProfile from "./user/EditProfile";
 
 export default function MainRouter() {
   return (
@@ -13,6 +15,7 @@ export default function MainRouter() {
       <Route path="/users" component={Users} />
       <Route path="/signup" component={SignUp} />
       <Route path="/signin" component={SignIn} />
+      <PrivateRoute path="/user/edit/:userId" component={EditProfile} />
       <Route path="/user/:userId" component={Profile} />
     </Switch>
   );
