79bb4bccfaf239636fce4572f88cb89c36543a65
yokomon <marowmars@gmail.com>
2020-11-04 20:00:06 +0100

    profile picture for users

--- client/user/EditProfile.js
+++ client/user/EditProfile.js
@@ -3,22 +3,22 @@ import { read, update } from "./api.user";
 import TextField from "@material-ui/core/TextField";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
-import Update from "@material-ui/icons/SystemUpdateOutlined";
-import Avatar from "@material-ui/core/Avatar";
 import Error from "@material-ui/icons/ErrorOutline";
 import { makeStyles } from "@material-ui/core/styles";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import { Redirect } from "react-router-dom";
 import { isAuthenticated } from "./../auth/auth";
+import Picture from "@material-ui/icons/PhotoSizeSelectActualRounded";
 
 const useStyles = makeStyles((theme) => ({
   input: {
     width: 320,
   },
   title: {
-    margin: theme.spacing(1),
     color: theme.palette.openTitle,
+    position: "relative",
+    top: "5px",
   },
   paper: {
     textAlign: "center",
@@ -27,6 +27,11 @@ const useStyles = makeStyles((theme) => ({
     position: "relative",
     top: theme.spacing(10),
   },
+  pic: {
+    position: "relative",
+    top: "15px",
+    color: "#e67c7c",
+  },
   content: {
     padding: "16px",
     paddingBottom: "24px",
@@ -34,12 +39,6 @@ const useStyles = makeStyles((theme) => ({
   btn: {
     margin: "auto",
     marginBottom: theme.spacing(3),
-  },
-  avatar: {
-    width: 50,
-    height: 50,
-    margin: "auto",
-    backgroundColor: "#e04343",
   },
   error: {
     verticalAlign: "middle",
@@ -56,6 +55,7 @@ export default function EditProfile({ match }) {
     email: "",
     error: "",
     about: "",
+    profile_picture: "",
     server_error: "",
     redirectToProfile: false,
   });
@@ -102,6 +102,10 @@ export default function EditProfile({ match }) {
             })
           : setValues({ ...values, error: "", [name]: value });
         break;
+      case "profile_picture":
+        let pic = event.target.files[0];
+        setValues({ ...values, error: "", [name]: pic });
+        break;
       case "email":
         value.match(/.+\@.+\..+/)
           ? setValues({ ...values, error: "", [name]: value })
@@ -139,13 +143,21 @@ export default function EditProfile({ match }) {
 
   return (
     <Paper elevation={3} className={classes.paper}>
+      <Typography variant={"h5"} className={classes.title}>
+        Edit profile
+      </Typography>
+      <input
+        accept="image/*"
+        type="file"
+        style={{ display: "none" }}
+        onChange={handleInput("profile_picture")}
+        id="profile_pic"
+      />
+      <label htmlFor="profile_pic">
+        <Picture className={classes.pic} />
+      </label>
+      <span>{values.profile_picture}</span>
       <CardContent>
-        <Avatar className={classes.avatar}>
-          <Update />
-        </Avatar>
-        <Typography variant={"h5"} className={classes.title}>
-          Edit profile
-        </Typography>
         <TextField
           name="Name"
           label="Name"
