8a63159f72fbc23bfdb839eac7cad711fe1a35c3
yokomon <marowmars@gmail.com>
2020-11-05 08:15:33 +0100

    server-side rendering

--- server/express.js
+++ server/express.js
@@ -7,9 +7,19 @@ import userRoutes from "./routes/user.routes";
 import authRoutes from "./routes/auth.routes";
 import { webpackCompile } from "./devBundle";
 import Template from "./../template";
+const path = require("path");
+const CWD = process.cwd();
 
 const app = express();
 // webpackCompile(app);
+
+// Server-side rendering
+import React from "react";
+import { ServerStyleSheets, ThemeProvider } from "@material-ui/core/styles";
+import { StaticRouter } from "react-router-dom";
+import ReactDOMServer from "react-dom/server";
+import MainRouter from "./../client/MainRouter";
+import theme from "./../client/theme";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -26,10 +36,31 @@ app.use(
 );
 app.use(compression());
 
+app.use("/dist", express.static(path.join(CWD, "dist")));
 app.use("/", userRoutes);
 app.use("/", authRoutes);
-app.use("/", (req, res) => {
-  return res.status(200).send(Template());
+app.get("*", (req, res) => {
+  const context = {};
+  const sheets = new ServerStyleSheets();
+  const markup = ReactDOMServer.renderToString(
+    sheets.collect(
+      <StaticRouter location={req.url} context={context}>
+        <ThemeProvider theme={theme}>
+          <MainRouter />
+        </ThemeProvider>
+      </StaticRouter>
+    )
+  );
+  if (context.url) {
+    return res.redirect(303, context.url);
+  }
+  const css = sheets.toString();
+  return res.status(200).send(
+    Template({
+      markup: markup,
+      css: css,
+    })
+  );
 });
 
 app.use((err, req, res, next) => {
